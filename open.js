40dc4a9aea591b670eda187780834e9cd77e02c8
Jean-Daniel Michaud <jeandaniel.michaud@ge.com>
2017-09-14 16:41:12 +0200

    Execute a select each time a new notepad is created to the user can start typing right away.

--- open.js
+++ open.js
@@ -2,7 +2,3 @@ chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.create({ url: "note.html" });
 });
 
-chrome.tabs.onCreated.addListener(function() {
-  // TODO: Select the textarea so the user can start typing right away
-});
-
